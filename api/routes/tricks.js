a77f3c6468d04e182e5e69f433c4666228efb2c1
santiset <sset@fi.uba.ar>
2022-04-04 18:55:32 -0300

    test trick in db

--- api/routes/tricks.js
+++ api/routes/tricks.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../db');
 const Tricks = require('../models/tricks')
 
+// Get all tricks
 router.get('/', async (req, res) => {
     let tricks = await Tricks.findAll();
     if (tricks) {
@@ -11,5 +12,30 @@ router.get('/', async (req, res) => {
         res.send("Error");
     }
 })
+
+// Add trick
+router.get("/add", (req, res) => {
+    const data = {
+        id: 1,
+        name: 'Ollie',
+        rating: 1,
+        description: 'Poping your skateboard off the ground.',
+        video: 'https://www.youtube.com/embed/VasSLuFO4wY',
+        type: 'Street'
+    }
+
+    let {id, name, rating, description, video, type} = data;
+
+    Tricks.create({
+        id,
+        name,
+        rating,
+        description,
+        video,
+        type
+    })
+    .then(trick => res.redirect('/tricks'))
+    .catch(err => console.log(err));
+});
 
 module.exports = router;

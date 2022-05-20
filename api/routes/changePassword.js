248fa046ea045a700d183b6398b0939744f35fa1
santiset <sset@fi.uba.ar>
2022-05-20 15:52:22 -0300

    bugs

--- api/routes/changePassword.js
+++ api/routes/changePassword.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const Users = require('../models/tricks')
+const {Users} = require('../models/users')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 router.post('/', async (req, res) => {
     let {user, pass} = req.body;
     try {
-        await Users.update(
-                { password : pass},
+        bcrypt.hash(pass, saltRounds, (err, hash) => {
+            if (err) {
+                console.log(err);
+            }
+            Users.update(
+                { password : hash },
                 { where: { username: user } }
             )
+        })
         res.send({message: `contrasenia modificada correctamente` , success: true});
     }
     catch(err){
